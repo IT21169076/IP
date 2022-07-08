ea11b0bba7dded3c7b0bca9a05f0a35779081a85
Shalini Muthumali Athukorala <99632841+IT21169076@users.noreply.github.com>
2022-07-08 09:04:41 +0530

    code c

---
+++ q22.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+
+
+int main(){
+	
+	char number[10];
+	
+	printf("Enter price for nexen tire : ");
+	scanf(" %s",&number);
+	
+//	float tot1 = number -  number*40/100;
+//	printf("%.2f ",tot1);
+printf("%s ",number);
+}
