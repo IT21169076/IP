96ba33356cd91e6fbbb011dc31446e016a296efa
Shalini Muthumali Athukorala <99632841+IT21169076@users.noreply.github.com>
2022-07-11 17:12:24 +0530

    code

---
+++ q1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void)
+{
+ int i ;
+ int upper = 0;
+ char myArray[100];
+ printf("input word : ");
+ scanf("%s" , myArray);
+ for(i=0; myArray[i]!= '\0'; i++){
+
+ if(myArray[i] % 2 == 0)
+ {
+ printf("%c\t" , myArray[i]);
+ }
+ else
+ {
+ printf("*\t");
+ }
+ }
+ return 0;
+}
