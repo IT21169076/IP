e5dfb1dd70827cf8ddabcd1de1be20c2c0cc381f
Shalini Muthumali Athukorala <99632841+IT21169076@users.noreply.github.com>
2022-07-31 09:12:43 +0530

    code

---
+++ q8.c
@@ -0,0 +1,42 @@
+# include <stdio.h>
+#include <string.h>
+
+void validPassword(char password[20]);
+int main(){
+	validPassword("Dhanuka");
+}
+
+void validPassword(char password[20]){
+	
+	printf(" %s\n",password);
+	int passwordLength = strlen(password);
+	int lower,upper,specialcharater,digit=0;
+	int length=0;
+	int i;
+	
+	for(i=0;i<passwordLength;i++){
+		if(password[i] >= 65 && password[i] <= 90){
+			upper++;
+		}
+		else if(password[i]>='a' && password[i]<='z'){
+			lower++;
+		}
+		else if(isdigit(password[i])){
+			digit++;
+		}
+		else if(password[i]=='@' || password[i]== '&' || password[i]== '$'){
+			specialcharater++;
+		}
+	}
+	printf("lower : %d  \n",lower);
+	printf("upper : %d \n",upper);
+	printf("digit : %d \n",digit);
+	printf("special : %d \n",specialcharater);
+	
+	if(length>10 && lower>1 && upper>1 && digit>1 && specialcharater>1){
+		printf("%s is Valied password.",password);
+	}else{
+		printf("%s is not Valied password.",password);
+	}
+
+}
