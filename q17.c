455ac6c4185db80c5fb9a2849a34f99cc6763c5e
Shalini Muthumali Athukorala <99632841+IT21169076@users.noreply.github.com>
2022-08-05 10:19:23 +0530

    code

---
+++ q17.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+//function main program execution 
+int main (void ){
+	int i ,num;
+	int myArr[8] ;
+	printf("Enter the number : ");
+	scanf("%d" , &num);
+	myArr[0] = num ;
+	printf("\n%d " , myArr[0]);
+	for (i = 1 ; i < 8 ; i++ )
+	{
+	myArr[i] = myArr[i-1] - (i*i) ;
+	printf("%d " , myArr[i]);
+	}
+return 0 ;
+} //end function main
+
